8e9465cc99e38385bd4d79c1741101bc2df3d727
김낙진 <nakjin@mail.shinhan.dev>
2022-02-20 21:42:00 +0900

    Initial Commit

---
+++ MyArchitecture-CommonUtility/MyArchitecture_CommonUtility.h
@@ -0,0 +1,18 @@
+//
+//  MyArchitecture_CommonUtility.h
+//  MyArchitecture-CommonUtility
+//
+//  Created by 60067670 on 2022/02/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyArchitecture_CommonUtility.
+FOUNDATION_EXPORT double MyArchitecture_CommonUtilityVersionNumber;
+
+//! Project version string for MyArchitecture_CommonUtility.
+FOUNDATION_EXPORT const unsigned char MyArchitecture_CommonUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyArchitecture_CommonUtility/PublicHeader.h>
+
+
